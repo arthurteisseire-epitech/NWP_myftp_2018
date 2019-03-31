612bedad3a7ea7ec1d9ca71de0289de71ff61318
arthur <arthur.teisseire@epitech.eu>
2019-03-31 18:03:29 +0200

    Infinite loop the accept and display client information

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@
 long int get_port(const char *port_str)
 {
     long port;
-    char *endptr = (char *)port_str;
+    char *endptr = (char *) port_str;
 
     port = strtol(port_str, &endptr, 10);
     if (port_str == endptr || port < 1 || port > 65535)
@@ -26,11 +26,13 @@ long int get_port(const char *port_str)
 int wait_connection(int fd)
 {
     int connfd;
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
 
-    listen(fd, 1);
-    connfd = accept(fd, NULL, NULL);
+    connfd = accept(fd, (struct sockaddr *) &client, &len);
     if (connfd < 0)
         exit_with("error when accepting");
+    dprintf(connfd, "ip : %s, port: %d\n", inet_ntoa(client.sin_addr), htons(client.sin_port));
     return connfd;
 }
 
@@ -38,13 +40,20 @@ int main(int ac, char *av[])
 {
     sock_t sock;
     int connfd;
+    char buffer[1024];
+    int rd_bytes;
 
     if (ac != 2)
         return (84);
     sock = create_socket(get_port(av[1]));
     bind_socket(&sock);
     printf("%s\n", inet_ntoa(sock.info.sin_addr));
-    connfd = wait_connection(sock.fd);
-    write(connfd, "Hello World!\n", sizeof("Hello World!\n"));
+    listen(sock.fd, 5);
+    while (1) {
+        connfd = wait_connection(sock.fd);
+        rd_bytes = read(connfd, buffer, 1024);
+        write(connfd, buffer, rd_bytes);
+        close(connfd);
+    }
     return (0);
 }
