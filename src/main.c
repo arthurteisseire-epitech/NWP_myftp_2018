d9f140fd3c7d2888c5b14d1df7b4ab5b1675abae
arthur <arthur.teisseire@epitech.eu>
2019-03-31 15:18:11 +0200

    Accept a connection and write on it

--- src/main.c
+++ src/main.c
@@ -5,20 +5,28 @@
 ** main.c
 */
 
+#include <stdio.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main()
 {
-    struct sockaddr_in addr;
+    struct sockaddr_in serv;
+    socklen_t len = sizeof(serv);
     int sockfd;
+    int connfd;
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = 2000;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv.sin_family = AF_INET;
+    serv.sin_port = htons(1234);
+    serv.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    printf("%s\n", inet_ntoa(serv.sin_addr));
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    bind(sockfd, (struct sockaddr *) &serv, len);
     listen(sockfd, 1);
+    connfd = accept(sockfd, NULL, NULL);
+    write(connfd, "Hello World!\n", sizeof("Hello World!\n"));
     return (0);
 }
