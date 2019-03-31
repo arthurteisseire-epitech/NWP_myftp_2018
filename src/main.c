905b3fbf4e86d3031ab517f9f04a3bf713249be8
arthur <arthur.teisseire@epitech.eu>
2019-03-31 13:30:37 +0200

    Create a sockaddr_in and bind it to socket

--- src/main.c
+++ src/main.c
@@ -7,8 +7,17 @@
 
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main()
 {
-    return socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
+    int sockfd;
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = 2000;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    return (0);
 }
