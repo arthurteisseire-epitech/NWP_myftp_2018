43380f8a6f01d2c0200ffe8b88e02a3254d8da87
arthur <arthur.teisseire@epitech.eu>
2019-04-04 11:45:08 +0200

    Add login incorrect message when user already connected

--- src/commands/command_user.c
+++ src/commands/command_user.c
@@ -17,8 +17,10 @@ int command_user(__attribute((unused))poll_t *poll,
 {
     char *p = find_second_arg(input);
 
-    if (conn->user.name && strcmp(conn->user.name, USERNAME) == 0)
+    if (conn->user.name && strcmp(conn->user.name, USERNAME) == 0) {
+        send_message(conn->sock.fd, CODE_LOGIN_INCORRECT);
         return (0);
+    }
     free(conn->user.name);
     conn->user.name = p;
     send_message(conn->sock.fd, CODE_USER);
