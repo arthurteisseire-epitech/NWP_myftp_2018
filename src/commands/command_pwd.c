7ee5c55a4e2944c66738e4b2fbc0d81d0f01600c
arthur <arthur.teisseire@epitech.eu>
2019-04-05 22:55:03 +0200

    Don't print last '/' in pwd command

--- src/commands/command_pwd.c
+++ src/commands/command_pwd.c
@@ -12,6 +12,11 @@
 int command_pwd(__attribute((unused))poll_t *poll, connection_t *conn,
     __attribute((unused))const char *input)
 {
-    dprintf(conn->sock.fd, "257 \"%s\"\r\n", conn->user.path);
+    char path[1024];
+
+    sprintf(path, "%s", conn->user.path);
+    if (strcmp(path, "/") != 0)
+        path[strlen(path) - 1] = '\0';
+    dprintf(conn->sock.fd, "257 \"%s\"\r\n", path);
     return (0);
 }
