52e6d083b5439bddad6550e0fe3a6230cf391b42
arthur <arthur.teisseire@epitech.eu>
2019-04-10 13:08:16 +0200

    Set accept_connection to coding style

--- src/connect.c
+++ src/connect.c
@@ -13,7 +13,8 @@
 void accept_connection(sock_t *sock)
 {
     sock->size_info = sizeof(sock->info);
-    sock->fd = accept(sock->fd, (struct sockaddr *) &sock->info, &sock->size_info);
+    sock->fd = accept(sock->fd, (struct sockaddr *) &sock->info,
+        &sock->size_info);
     if (sock->fd < 0)
         exit_with("accept: %s", strerror(errno));
 }
