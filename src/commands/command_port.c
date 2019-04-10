6142a8972b0e40bc201173a4a183c01f5e73860e
arthur <arthur.teisseire@epitech.eu>
2019-04-10 10:32:44 +0200

    Set port for port command

--- src/commands/command_port.c
+++ src/commands/command_port.c
@@ -34,6 +34,16 @@ static char *split_arg(char *arg)
     return (substr);
 }
 
+static in_port_t get_port(char *str)
+{
+    int left = strtol(str, &str, 10);
+    int right;
+
+    ++str;
+    right = strtol(str, &str, 10);
+    return (htons(left * 256 + right));
+}
+
 int command_port(__attribute((unused))poll_t *poll, connection_t *conn,
     const char *input)
 {
@@ -42,9 +52,12 @@ int command_port(__attribute((unused))poll_t *poll, connection_t *conn,
 
     if (port_str == NULL ||
         !inet_aton(ip_str, (struct in_addr *) &conn->data_sock.info)) {
-        send_message(conn->sock.fd, CODE_COMMAND_NOT_FOUND, "Invalid port command.");
+        send_message(conn->sock.fd, CODE_COMMAND_NOT_FOUND,
+            "Invalid port command.");
         return (-1);
     }
+    conn->data_sock.size_info = sizeof(conn->data_sock.info);
+    conn->data_sock.info.sin_port = get_port(port_str);
     send_message(conn->sock.fd, CODE_SUCCESS,
         "PORT command successful. Consider using PASV.");
     conn->mode = ACTIVE;
